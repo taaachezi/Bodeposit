0a902c1781039f900aa35350c209216181d42f1d
taaachezi <taaachezi@gmail.com>
2020-09-08 02:52:37 +0000

    [add]material

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,20 +1,18 @@
 class GenresController < ApplicationController
+  before_action :set_method
   def new
-  	genre = Genre.find_by(id: params[:id])
-  	@genres = current_user.genres
-  	@addgenre = Genre.new
   end
 
   def create
-  	@addgenre = Genre.new(params_addgenre)
+  	@addgenre = Genre.new(params_genre)
   	@addgenre.user_id = current_user.id
   	@addgenre.save
   	flash[:notice] = "ジャンルを追加しました"
   end
 
   def update
-  	genre = Genre.find_by(id: params[:id])
-  	genre.update(params_genre)
+    @addgenre = Genre.new
+  	@genre.update(params_genre)
   	flash[:notice] = "ジャンルを変更しました"
   end
 
@@ -22,6 +20,12 @@ class GenresController < ApplicationController
 
   def params_genre
   	params.require(:genre).permit(:name, :status)
+  end
+
+  def set_method
+    @genre = Genre.find_by(id: params[:id])
+    @genres = current_user.genres
+    @addgenre = Genre.new
   end
 
 end
--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,4 +1,32 @@
 class MaterialsController < ApplicationController
   def index
+  	@addmaterial = Material.new
+  	@materials = current_user.materials
   end
+
+  def create
+  	@addmaterial = Material.new(params_material)
+  	@addmaterial.user_id = current_user.id
+  	@addmaterial.calorie = calorie_fit(@addmaterial.fat, @addmaterial.protein, @addmaterial.carbohydrate)
+  	@addmaterial.save
+  	flash[:notice] = "材料を登録しました"
+  	redirect_back(fallback_location: root_path)
+  end
+
+  def update
+  	material = Material.find(params[:id])
+  	material.update(params_material)
+  	redirect_back(fallback_location: root_path)
+  end
+
+  def search
+  	
+  end
+
+  private
+
+  def params_material
+  	params.require(:material).permit(:name, :genre_id, :fat, :protein, :carbohydrate, :calorie)
+  end
+ 
 end
