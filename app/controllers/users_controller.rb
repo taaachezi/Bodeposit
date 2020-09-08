226460f289ac6ca2d98154380ed027375e2de416
taaachezi <taaachezi@gmail.com>
2020-09-08 14:18:20 +0000

    [add]material,user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,30 @@
 class UsersController < ApplicationController
-  def top
-  end
 
-  def index
-  end
+  before_action :set_user
 
   def show
   end
 
-  def edit
-  end
-
   def update
+    @user.update(params_users)
+    redirect_back(fallback_location: root_path)
   end
 
   def unsubscribe
+    current_user.destroy
+    redirect_to @user
   end
 
   def withdraw
+  end
+
+  private
+
+  def params_users
+    params.require(:user).permit(:name, :email, :sex, :age, :height, :weight, :level)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
