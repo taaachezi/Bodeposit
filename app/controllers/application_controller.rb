3c7c207fce92a211db929898963f7a3b9de53db6
taaachezi <taaachezi@gmail.com>
2020-09-14 07:07:28 +0000

    [fix]assets_file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
 
 	protected
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :height, :weight, :level, :age, :sex,])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :height, :weight, :level, :age, :sex,])
 	end
 
 end
