1b9e699945d6cb6bdfeb60371a39c23985f1de84
taaachezi <taaachezi@gmail.com>
2020-09-27 13:38:44 +0000

    [fix]routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
       get :withdraw
       patch :unsubscribe
     end
-    resources :eats, only: [:create, :destroy] do
+    resources :eats, only: [:create, :destroy]
   end
 
   resource :genres, only: [:new, :create, :update, :destroy] do
