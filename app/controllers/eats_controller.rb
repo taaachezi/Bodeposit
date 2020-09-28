32fe1058e50738ae17cc0161b12c79bab2e681f8
taaachezi <taaachezi@gmail.com>
2020-09-28 06:04:56 +0000

    [fix]eat_ajax

--- app/controllers/eats_controller.rb
+++ app/controllers/eats_controller.rb
@@ -16,8 +16,9 @@ class EatsController < ApplicationController
     else
       # materialを摂取する
       @eat = current_user.eats.new(params_eat)
-      if params[:eat][:material_id] == "" || params[:eat][:quantity] == ""
+      if params[:eat][:material_id].empty? || params[:eat][:quantity].empty?
         flash[:error] = "材料または数量が選択されていません"
+        redirect_back(fallback_location: root_path)
       else
         @eat.material_id = @eat.material.id
         @eat.protein = @eat.material.protein * @eat.quantity / 100
@@ -25,17 +26,7 @@ class EatsController < ApplicationController
         @eat.carbohydrate = @eat.material.carbohydrate * @eat.quantity / 100
         @eat.calorie = Material.calorie_fit(@eat.fat, @eat.protein, @eat.carbohydrate)
         @eat.save
-        @eat_calorie = 0
-        @eat_protein = 0
-        @eat_carbo = 0
-        @eat_fat = 0
-        @eats = current_user.eats.page(params[:page]).per(5)
-        @eats.each do |eat|
-          @eat_calorie += eat.calorie.to_f
-          @eat_protein += eat.protein.to_f
-          @eat_carbo += eat.carbohydrate.to_f
-          @eat_fat += eat.fat.to_f
-        end
+        params_calorie
         flash[:notice] = "#{@eat.material.name}を摂取しました"
       end
     end
@@ -44,11 +35,28 @@ class EatsController < ApplicationController
   def destroy
     eat = Eat.find_by(id: params[:id])
     eat.destroy
+    @eat = current_user.eats.new
+    params_calorie
     flash[:notice] = "削除しました"
-    redirect_back(fallback_location: root_path)
   end
+
+  private
 
   def params_eat
     params.require(:eat).permit(:material_id, :protein, :fat, :carbohydrate, :calorie, :quantity)
+  end
+
+  def params_calorie
+    @eat_calorie = 0
+    @eat_protein = 0
+    @eat_carbo = 0
+    @eat_fat = 0
+    @eats = current_user.eats.page(params[:page]).per(5)
+    @eats.each do |eat|
+      @eat_calorie += eat.calorie.to_f
+      @eat_protein += eat.protein.to_f
+      @eat_carbo += eat.carbohydrate.to_f
+      @eat_fat += eat.fat.to_f
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def recipe_material_calorie(calorie, quantity)
-    calorie * quantity / 100
+    calorie.to_f * quantity / 100
   end
 
   def weight_gain_calorie(calorie)
@@ -12,23 +12,23 @@ module ApplicationHelper
   end
 
   def weight_gain_carbo(calorie, fat, protein)
-    calorie += 500
+    calorie.to_f += 500
     (calorie - (protein * 4 + fat * 9)) / 4
   end
 
   def weight_slim_carbo(calorie, fat, protein)
     calorie -= 500
-    (calorie - (protein * 4 + fat * 9)) / 4
+    (calorie.to_f - (protein * 4 + fat * 9)) / 4
   end
 
   def target_calorie_gain(eat_calorie, user_calorie)
     user_calorie = user_calorie.to_f + 500
-    user_calorie - eat_calorie.to_f
+    user_calorie.to_f - eat_calorie.to_f
   end
 
   def target_calorie_slim(eat_calorie, user_calorie)
     user_calorie = user_calorie.to_f - 500
-    user_calorie - eat_calorie.to_f
+    user_calorie.to_f - eat_calorie.to_f
   end
 
   def target_protein(eat_protein, user_protein)
@@ -42,12 +42,12 @@ module ApplicationHelper
   def target_carbo_gain(eat_carbo, calorie, fat, protein)
     calorie = calorie.to_f + 500
     user_carbo = (calorie - (protein.to_f * 4 + fat.to_f * 9)) / 4
-    user_carbo - eat_carbo
+    user_carbo.to_f - eat_carbo.to_f
   end
 
   def target_carbo_slim(eat_carbo, calorie, fat, protein)
     calorie = calorie.to_f - 500
     user_carbo = (calorie - (protein.to_f * 4 + fat.to_f * 9)) / 4
-    user_carbo - eat_carbo
+    user_carbo.to_f - eat_carbo.to_f
   end
 end
